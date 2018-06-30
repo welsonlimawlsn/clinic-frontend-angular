3f49b9215af39ea02ca02e252925756bc3da9497
Welson <welsonlimawlsn@gmail.com>
2018-06-30 14:31:36 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'clinic-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'clinic';
+}
